fe61587152245f370e8429951d1fd61b13f97476
Rössger Dirk <dirk.rossger@afconsult.com>
2017-11-23 08:54:49 +0100

    Active.Editor.WriteMessage("\n-> Get vertex points from MeshObject: xMeshPoints");
                Active.Editor.WriteMessage("\n-> Get enclosed Polyline from Point cloud: xEnclosePoints_Polyline");
                Active.Editor.WriteMessage("\n-> Get enclosed Polyline as Rectangle from Point cloud: xEnclosePoints_Rectangle");
                Active.Editor.WriteMessage("\n-> Create Civil3d Tinsurface from Mesh: xTinsurfaceFromMesh");
    
    + Dwg samples in Civil3d 2017

---
+++ GetVerticesFromSubDMesh/cActive.cs
@@ -0,0 +1,61 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.ApplicationServices.Core;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+
+using Autodesk.Civil.ApplicationServices;
+using Autodesk.Civil.DatabaseServices;
+
+
+namespace GetVerticesFromSubDMesh
+{
+	/// <summary>
+	/// Provides easy access to several "active" objects in the AutoCAD
+	/// runtime environment.
+	/// </summary>
+	public static class Active
+	{
+		/// <summary>
+		/// Returns the active Editor object.
+		/// </summary>
+		public static Editor Editor
+		{
+			get { return Document.Editor; }
+		}
+
+		/// <summary>
+		/// Returns the active Document object.
+		/// </summary>
+		public static Document Document
+		{
+			get { return Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument; }
+		}
+
+        /// <summary>
+        /// Returns the active Database object.
+        /// </summary>
+        public static Database Database
+		{
+			get { return Document.Database; }
+		}
+
+		/// <summary>
+		/// Sends a string to the command line in the active Editor
+		/// </summary>
+		/// <param name="message">The message to send.</param>
+		public static void WriteMessage(string message)
+		{
+			Editor.WriteMessage(message);
+		}
+
+		/// <summary>
+		/// Sends a string to the command line in the active Editor using String.Format.
+		/// </summary>
+		/// <param name="message">The message containing format specifications.</param>
+		/// <param name="parameter">The variables to substitute into the format string.</param>
+		public static void WriteMessage(string message, params object[] parameter)
+		{
+			Editor.WriteMessage(message, parameter);
+		}
+	}
+}
---
+++ GetVerticesFromSubDMesh/cExtension.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.ApplicationServices.Core;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+
+[assembly: CommandClass(typeof(GetVerticesFromSubDMesh.GetVerticesFromSubDMesh))]
+
+namespace GetVerticesFromSubDMesh
+{
+    public class GetVerticesFromSubDMesh : IExtensionApplication
+    {
+        [CommandMethod("info")]
+        public void Initialize()
+        {
+            Active.Editor.WriteMessage("\n-> Get vertex points from MeshObject: xMeshPoints");
+            Active.Editor.WriteMessage("\n-> Get enclosed Polyline from Point cloud: xEnclosePoints_Polyline");
+            Active.Editor.WriteMessage("\n-> Get enclosed Polyline as Rectangle from Point cloud: xEnclosePoints_Rectangle");
+            Active.Editor.WriteMessage("\n-> Create Civil3d Tinsurface from Mesh: xTinsurfaceFromMesh");
+        }
+
+        public void Terminate()
+        {
+        }
+    }
+}
